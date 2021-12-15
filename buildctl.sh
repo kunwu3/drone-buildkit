5022ad1c7eaed276cae4d3d8a833ab62fdc37c66
kunwu3 <krisvictor3@gmail.com>
2021-12-15 10:50:27 +0800

    Add files via upload

--- buildctl.sh
+++ buildctl.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [ -z ${PLUGIN_CTLADDR} ]; then
-  PLUGIN_CTLADDR="tcp://0.0.0.0:1234"
+if [ -z ${PLUGIN_ADDR} ]; then
+  exit 1
 fi
 
-if [ -z ${PLUGIN_IMAGE} ]; then
-  PLUGIN_IMAGE="harbor.eswin.cn/ci-tool/fail-build"
+if [ -z ${PLUGIN_REPO} ]; then
+  exit 1
+fi
+
+if [ -z ${PLUGIN_REGISTRY} ]; then
+  PLUGIN_REGISTRY="docker.io"
 fi
 
 if [ -z ${PLUGIN_TAG} ]; then
@@ -13,16 +17,27 @@ if [ -z ${PLUGIN_TAG} ]; then
 fi
 
 if [ -z ${PLUGIN_INSECURE} ]; then
-  PLUGIN_TAG="false"
+  PLUGIN_INSECURE="false"
 fi
 
+if [ -z ${PLUGIN_DOCKERSRC} ]; then
+  PLUGIN_DOCKERSRC=""
+fi
+
+if [ -z ${PLUGIN_AUTH} ]; then
+  PLUGIN_AUTH=""
+fi
+
+echo ${PLUGIN_AUTH} > /root/.docker/config.json
+
+echo "Pushing image to ${PLUGIN_REGISTRY}/${PLUGIN_REPO}:${PLUGIN_TAG}"
 
 buildctl \
-    --addr ${PLUGIN_CTLADDR} \
+    --addr ${PLUGIN_ADDR} \
     build \
     --frontend=dockerfile.v0 \
     --local context=. \
-    --local dockerfile=. \
-    --output type=image,push=true,name=${PLUGIN_IMAGE}:${PLUGIN_TAG},registry.insecure=${PLUGIN_INSECURE} \
-    --import-cache type=local,src=/cache
-    --export-cache type=local,src=/cache
+    --local dockerfile=./${PLUGIN_DOCKERSRC} \
+    --output type=image,push=true,name=${PLUGIN_REGISTRY}/${PLUGIN_REPO}:${PLUGIN_TAG},registry.insecure=${PLUGIN_INSECURE} \
+    --export-cache type=local,dest=./image-build-cache \
+    --import-cache type=local,src=./image-build-cache
